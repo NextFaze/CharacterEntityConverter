2e849af3a851d8ef4dd20216d3f2d2de5c5d052e
Andrew Williams <awilliams@2moro.com.au>
2011-03-18 12:13:29 +1030

    Initial Commit

---
+++ CharacterEntityConverterTests/CharacterEntityConverterTests.h
@@ -0,0 +1,17 @@
+//
+//  CharacterEntityConverterTests.h
+//  CharacterEntityConverterTests
+//
+//  Created by Andrew Williams on 18/03/11.
+//  Copyright 2011 2moro mobile. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface CharacterEntityConverterTests : SenTestCase {
+@private
+    
+}
+
+@end
---
+++ CharacterEntityConverterTests/CharacterEntityConverterTests.m
@@ -0,0 +1,33 @@
+//
+//  CharacterEntityConverterTests.m
+//  CharacterEntityConverterTests
+//
+//  Created by Andrew Williams on 18/03/11.
+//  Copyright 2011 2moro mobile. All rights reserved.
+//
+
+#import "CharacterEntityConverterTests.h"
+
+
+@implementation CharacterEntityConverterTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in CharacterEntityConverterTests");
+}
+
+@end
